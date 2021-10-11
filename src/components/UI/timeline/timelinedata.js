3a8e9aaf1887e009ff90113bc144b08cfd939431
codetective <iaminiodu@gmail.com>
2021-10-11 21:29:39 +0100

    added content on homepage

--- src/components/UI/timeline/timelinedata.js
+++ src/components/UI/timeline/timelinedata.js
@@ -1,59 +1,39 @@
 const timelinedata = [
   {
-    text: "Wrote my first blog post ever on Medium",
-    date: "March 03 2017",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+    text: "PANS Election Campaign",
+    date: "October 10 2020",
     // link: {
     //   url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
     //   text: "Read more",
     // },
   },
   {
-    text: "Wrote my first blog post ever on Medium",
-    date: "March 03 2017",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+    text: "PANS Election and Voting",
+    date: "October 10 2020",
     // link: {
     //   url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
     //   text: "Read more",
     // },
   },
   {
-    text: "Wrote my first blog post ever on Medium",
-    date: "March 03 2017",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+    text: "PANS Executives inauguration",
+    date: "October 10 2020",
     // link: {
     //   url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
     //   text: "Read more",
     // },
   },
   {
-    text: "Wrote my first blog post ever on Medium",
-    date: "March 03 2017",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+    text: "Uniuyo Pharmacy exams begins",
+    date: "October 10 2020",
     // link: {
     //   url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
     //   text: "Read more",
     // },
   },
   {
-    text: "Wrote my first blog post ever on Medium",
-    date: "March 03 2017",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+    text: "End of Second semester",
+    date: "October 10 2020",
     // link: {
     //   url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
     //   text: "Read more",
